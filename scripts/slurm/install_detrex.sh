f5b9e7154fe80a8d965537ddc8b764ca4ce4005d
Diego Canez <canezdiego@gmail.com>
2024-07-31 18:29:26 +0200

    feat: add install scripts

---
+++ scripts/slurm/install_detrex.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --job-name=install
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=00:20:00
+#SBATCH --output=scripts/slurm_logs/slurm_output_%A.out
+
+cd $HOME/development/edge
+source .venv/bin/activate
+
+cd $HOME/development/edge/detrex
+srun pip install -e .
+
+cd $HOME/development/edge
+srun pip install -e .
+deactivate
