7a1fb6dc490708b31b1bb4c40b675262ed87614c
Diego Canez <canezdiego@gmail.com>
2024-08-02 16:10:27 +0200

    feat: add try_train slurm scripts and decrease the checkpoint period

--- projects/dino_dinov2/configs/COCO/dino_dinov2_b_12ep.py
+++ projects/dino_dinov2/configs/COCO/dino_dinov2_b_12ep.py
@@ -16,14 +16,14 @@ train.output_dir = "./output/dino_vitdet_base_12ep"
 # max training iterations
 train.max_iter = 90000
 
-# run evaluation every 5000 iters
-train.eval_period = 5000
+# run evaluation every 2500 iters
+train.eval_period = 2500
 
 # log training infomation every 20 iters
 train.log_period = 20
 
-# save checkpoint every 5000 iters
-train.checkpointer.period = 5000
+# save checkpoint every 2500 iters
+train.checkpointer.period = 2500
 
 # gradient clipping for training
 train.clip_grad.enabled = True
@@ -42,13 +42,13 @@ optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in modul
 
 # modify dataloader config
 # dataloader.train.num_workers = 16
-dataloader.train.num_workers = 8
+dataloader.train.num_workers = 16
 
 # please notice that this is total batch size.
 # surpose you're using 4 gpus for training and the batch size for
 # each gpu is 16/4 = 4
 # dataloader.train.total_batch_size = 16
-dataloader.train.total_batch_size = 8
+dataloader.train.total_batch_size = 16
 
 # dump the testing results into output_dir for visualization
 dataloader.evaluator.output_dir = train.output_dir
--- scripts/slurm/try_train.sh
+++ scripts/slurm/try_train.sh
@@ -4,8 +4,8 @@
 #SBATCH --gpus=4
 #SBATCH --job-name=train
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=72
-#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=18
+#SBATCH --time=00:10:00
 #SBATCH --output=scripts/slurm_logs/slurm_output_%A.out
 
 module load 2023
@@ -19,7 +19,10 @@ cd $HOME/development/edge/detrex
 export DETECTRON2_DATASETS=$HOME/datasets
 python tools/train_net.py \
     --config-file ../projects/dino_dinov2/configs/COCO/dino_dinov2_b_12ep.py \
-    --num-gpus 4 
+    --num-gpus 4 \
+    train.fast_dev_run.enabled=True
+
+
 
 deactivate
 module unload CUDA/12.1.1
